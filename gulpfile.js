2de3d773a8d559df843a0c4862501f301eac864c
Jamal Aha <jamalah@icloud.com>
2021-08-08 07:49:12 +0300

    commit with postscss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 var sass = require('gulp-sass')(require('sass'));
 
 sass.compiler = require('node-sass');
@@ -8,9 +11,12 @@ sass.compiler = require('node-sass');
 gulp.task('scss', function () {
     return gulp.src('./src//*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            cssnano()
+        ]))
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('scss:watch', function () {
-    gulp.watch('./src/**/*.scss',  gulp.series(['scss']));
+    gulp.watch('./src/**/*.scss', gulp.series(['scss']));
 });
